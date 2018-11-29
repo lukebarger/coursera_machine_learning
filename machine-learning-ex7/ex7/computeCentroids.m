1e8309816676b2d9ba2f4bdf12e2e7a89ee3e86d
lukebarger <42392715+lukebarger@users.noreply.github.com>
2018-11-29 07:42:39 -0700

    Add files via upload
    Stanford ML coursera course exercise 7
    K-means clustering
    Principal component analysis (PCA)
    
    Applies k-means clustering to compress an image down to 16 colors
    Applies PCA to compress (and recover) an image set of faces

---
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -0,0 +1,46 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+for ii = 1:K
+  asgn = idx==ii;  % 1's if assnged to this centroid, 0's otherwise
+  % 1/sum(asgn) divides by the number of venctors assigned to that centroid
+  % asgn'*X  adds the vectors assigned to the centroid
+  centroids(ii,:) = (1/sum(asgn))*asgn'*X;
+  
+endfor
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
